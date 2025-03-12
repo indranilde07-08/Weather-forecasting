c508eccd3e2aa7891f4c337f419ccbbc8f9c92d1
indranild de <84306058+indranil07-8@users.noreply.github.com>
2025-03-13 00:17:49 +0530

    complete weather forcasting website

--- backend.py
+++ backend.py
@@ -3,16 +3,12 @@ import requests
 API_KEY = "35eed2c976138f3a4d414906aa78577d"
 
 
-def get_data(place, days=None, label=None):
+def get_data(place, days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     filtered_data = filtered_data[:8*days]
-    if label == "Temperature":
-        filtered_data =  [dict["main"]["temp"]for dict in filtered_data]
-    elif label == "Sky":
-        filtered_data = [dict["weather"][0]["main"]for dict in filtered_data]
     return filtered_data
 
 
@@ -20,4 +16,4 @@ def get_data(place, days=None, label=None):
 
 
 if __name__ == "__main__":
-    print(get_data(place="mundra",days=3,label="Temperature"))
+    print(get_data(place="mundra",days=3))
--- main.py
+++ main.py
@@ -1,18 +1,100 @@
+import base64
+
 import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+
+# Add background image
+def set_background(image_url):
+    with open(image_url, "rb") as file:  # read data into binnary from
+        encode_string = base64.b64encode(file.read()).decode()  # binnary to convert base64 then bas64 decode as string
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+        background-image: url("data:png;base64,{encode_string}");
+        background-size:cover;
+        background-position: center;
+        background-attachment: fixed;
+    }}
+        </style>
+
+""",
+        unsafe_allow_html=True
+    )
+
+
+set_background("7280763.png")
+
+
+# Convert wind direction from degrees to compass directions
+def get_wind_direction(deg):
+    directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+    idx = round(deg / 45) % 8
+    return directions[idx]
+
+
+# Add title,text input,slider,selectbox and subhead er
 st.title("Weather Forcast for the Next Days")
 place = st.text_input("Place:")
 days = st.slider("Forcast Days", min_value=1, max_value=5
                  , help="Select the number of forecasted days")
-option = st.selectbox("Select data to view", ("Temperature", "Sky"))
+option = st.selectbox("Select data to view", ("Temperature", "Sky", "Humidity", "Wind-speed"))
 
-st.subheader(f"{option} for the next {days} days in{place}")
+st.subheader(f"{option} for the next {days} days in {place}")
+
+if place:
+    # Get the temperature/sky data
+    filtered_data = get_data(place, days)
+
+    # Create a temperature plot
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        temperature = [temperature - 273 for temperature in temperatures]
+        Dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=Dates, y=temperature, labels={"x": "Dates", "y": "Temperatures (C)"})
+        st.plotly_chart(figure)
+
+    elif option == "Sky":
+        # define day and night images
+        images = {
+            "Clear": {"day": "images/clear.png", "night": "images/night.png"},
+            "Clouds": {"day": "images/cloud.png", "night": "images/cloudy-night.png"},
+            "Rain": {"day": "images/rain.png", "night": "images/rainy.png"},
+            "Snow": {"day": "images/snow.png", "night": "images/snow_night.png"},
+        }
+        # Extract weather condition and timestamps
+        sky_conditons = [dict["weather"][0]["main"] for dict in filtered_data]
+        timesstamps = [dict["dt_txt"] for dict in filtered_data]
+
+        # Determine if it's day or night
+        images_path = []
+        for i, condition in enumerate(sky_conditons):
+            hour = int(timesstamps[i].split(" ")[1].split(":")[0])  # Extract hour
+            time_of_day = "day" if 6 <= hour < 18 else "night"
+            images_path.append(images[condition][time_of_day])
+
+        st.image(images_path, width=115)
 
 
 
-
-d, t = get_data(place,days,option)
-figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperatures (C)"})
-st.plotly_chart(figure)
+    elif option == "Humidity":
+        humidity = [dict["main"]["humidity"] for dict in filtered_data]
+        Dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.bar(x=Dates, y=humidity, labels={"x": "Dates", "y": "Humidity"},
+                        color_discrete_sequence=["#FF5733"])
+        st.plotly_chart(figure)
+    elif option == "Wind-speed":
+        wind_speed = [dict['wind']['speed'] for dict in filtered_data]
+        wind_Speed = [i * 3.6 for i in wind_speed] # convert m/s to km/h
+        wind_deg = [get_wind_direction(dict['wind']['deg']) for dict in filtered_data]
+        ploat = px.bar_polar(r=wind_Speed, theta=wind_deg, title="wind speed & Direction ")
+        st.plotly_chart(ploat)
+    if option == "Wind-speed":
+        wind_speed = [dict['wind']['speed'] for dict in filtered_data]
+        wind_Speed = [i * 3.6 for i in wind_speed]  # convert m/s to km/h
+        Dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.scatter(x=Dates, y=wind_Speed, labels={"x": "Dates", "y": "wind-speed"},
+                            color_discrete_sequence=["#FF5733"])
+        st.plotly_chart(figure)
